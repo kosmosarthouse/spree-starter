91951bdabecdbd4195b72608737bde27238a2474
kosmosarthouse <kosmosarthouse@gmail.com>
2026-06-30 05:34:29 +0530

    Update spree.rb

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,58 +1,24 @@
 # frozen_string_literal: true
 
-# Configure Spree Preferences
-#
-# Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
-#       If you would like users to be able to update a setting with the Admin it should NOT be set here.
-#
-# Note: If a preference is set here it will be stored within the cache & database upon initialization.
-#       Just removing an entry from this initializer will not make the preference value go away.
-#       Instead you must either set a new value or remove entry, clear cache, and remove database entry.
-#
-# In order to initialize a setting do:
-# config.setting_name = 'new value'
-#
-# More on configuring Spree preferences can be found at:
-# https://docs.spreecommerce.org/developer/customization
 Spree.config do |config|
-  # Example:
-  # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
 end
 
-# Configure Spree Dependencies
-#
-# Note: If a dependency is set here it will NOT be stored within the cache & database upon initialization.
-#       Just removing an entry from this initializer will make the dependency value go away.
-#
-# More on how to use Spree dependencies can be found at:
-# https://docs.spreecommerce.org/customization/dependencies
 Spree.dependencies do |dependencies|
-  # Example:
-  # Uncomment to change the default Service handling adding Items to Cart
   # dependencies.cart_add_item_service = 'MyNewAwesomeService'
 end
 
 Rails.application.config.after_initialize do
-  # Spree.shipping_methods << Spree::ShippingMethods::SuperExpensiveNotVeryFastShipping
-  # Spree.payment_methods << Spree::PaymentMethods::VerySafeAndReliablePaymentMethod
+  # Fix Spree engine report download URLs
+  Spree::Core::Engine.routes.default_url_options = {
+    host:     "spree.nozfragrances.com",
+    protocol: "https"
+  }
 
-  # Spree.calculators.tax_rates << Spree::TaxRates::FinanceTeamForcedMeToCodeThis
-
-  # Spree.stock_splitters << Spree::Stock::Splitters::SecretLogicSplitter
-
-  # Spree.adjusters << Spree::Adjustable::Adjuster::TaxTheRich
-
-  # Custom promotions
-  # Spree.calculators.promotion_actions_create_adjustments << Spree::Calculators::PromotionActions::CreateAdjustments::AddDiscountForFriends
-  # Spree.calculators.promotion_actions_create_item_adjustments << Spree::Calculators::PromotionActions::CreateItemAdjustments::FinanceTeamForcedMeToCodeThis
-  # Spree.promotions.rules << Spree::Promotions::Rules::OnlyForVIPCustomers
-  # Spree.promotions.actions << Spree::Promotions::Actions::GiftWithPurchase
-
-  # Spree.taxon_rules << Spree::TaxonRules::ProductsWithColor
-
-  # Spree.exports << Spree::Exports::Payments
-  # Spree.reports << Spree::Reports::MassivelyOvercomplexReportForCfo
+  Rails.application.routes.default_url_options = {
+    host:     "spree.nozfragrances.com",
+    protocol: "https"
+  }
 
   # Role-based permissions
   Spree.permissions.assign(:default, [Spree::PermissionSets::DefaultCustomer])
@@ -81,7 +47,6 @@ Spree.queues.payment_webhooks = :spree_payment_webhooks
 Spree.queues.api_keys = :spree_api_keys
 Spree.queues.search = :spree_search
 
-# Search provider
 if ENV['MEILISEARCH_URL'].present?
   Spree.search_provider = 'Spree::SearchProvider::Meilisearch'
 end
